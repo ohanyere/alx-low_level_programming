26a09c9c691ef73a6e5549402924b1daf3b86334
ohanyere <mooref068@gmail.com>
2022-07-12 05:01:15 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H
+#define _MAIN_H_
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c:The character to be print
+ * Return: 0
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
